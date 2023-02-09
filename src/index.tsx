c317791e42214d40819befca0d475d94820d4b96
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-09 21:43:22 +0100

    Add context wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./core/components/app/App";
+import { PhoneContextProvider } from "./core/context/phone-context-provider";
+import "./index.scss";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <PhoneContextProvider>
+      <App />
+    </PhoneContextProvider>
   </React.StrictMode>
 );
 
